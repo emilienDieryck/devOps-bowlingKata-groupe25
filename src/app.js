fa4d300f3cb7ae1643a5c6c929358d1c5d934fa4
Emilien <emilien.dieryck@student.vinci.be>
2022-11-20 15:27:24 +0100

    feat(app): webpack setup and app.js addition

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const getCurrentMonth = (date = new Date()) => {
+    const currentMonth = date.getMonth();
+
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ]
+
+    return months[currentMonth];
+};
+
+try{
+    document.getElementById("month").innerText = getCurrentMonth();
+}catch (err) {}
+
+exports.getCurrentMonth = getCurrentMonth();
