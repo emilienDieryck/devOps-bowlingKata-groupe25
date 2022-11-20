a7852d2cc174ff7575509fdcdd1197bd1923d017
Emilien <emilien.dieryck@student.vinci.be>
2022-11-20 19:36:52 +0100

    fix(app): app.test.js chage const result = getCurrentMonth without ()

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -2,7 +2,7 @@ const { getCurrentMonth } = require("../app");
 
 describe("app tests suites - getCurrentMonth", () => {
   test("should return the current month", () => {
-    const result = getCurrentMonth();
+    const result = getCurrentMonth;
     expect(result).toBe("November");
   });
 });
