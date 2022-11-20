46bec14ab3e5e712bca58c35e5835ddb468b7394
Emilien <emilien.dieryck@student.vinci.be>
2022-11-20 16:43:32 +0100

    feat(app): adding folder tests & app.test & jest

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,8 @@
+const { getCurrentMonth } = require('../app');
+
+describe('app tests suites - getCurrentMonth', () => {
+    test('should return the current month', () => {
+        const result = getCurrentMonth();
+        expect(result).toBe('November');
+    });
+});
