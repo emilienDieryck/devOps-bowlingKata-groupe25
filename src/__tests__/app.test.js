b372db08aedef289b6ed6807b36ca15f8ebe0005
Emilien <emilien.dieryck@student.vinci.be>
2022-11-24 15:28:18 +0100

    feat(app): add file app and app.test

--- src/__tests__/app.test.js
+++
@@ -1,8 +0,0 @@
-const { bowlingKata , roll , score } = require('../app');
-
-
-describe("app test the roll ", () => {
-  test("should return the current month", () => {
-    expect(roll(3)).toBe(3);
-  });
-});
