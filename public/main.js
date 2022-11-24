56e0dbb2a5a23c61f9bbf03783cc5d656418ea03
Tanguy Vandervelden <tanguy.vandervelden@student.vinci.be>
2022-11-24 18:18:29 +0100

    style: fixing code

--- public/main.js
+++ public/main.js
@@ -1,1 +1,6 @@
+<<<<<<< Updated upstream
 (()=>{const e=(e=new Date)=>["January","February","March","April","May","June","July","August","September","October","November","December"][e.getMonth()];try{document.getElementById("month").innerText=e();}catch(e){}e();})();
+=======
+(()=>{const e=(e=new Date)=>["January","February","March","April","May","June","July","August","September","October","November","December"][e.getMonth()];
+try{document.getElementById("month").innerText=e()}catch(e){}e()})();
+>>>>>>> Stashed changes
